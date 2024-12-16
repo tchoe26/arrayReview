f233fa6a312e2f733f991f361e18b7216f782ef6
teddy <theodore_choe26@milton.edu>
2024-12-16 10:14:18 -0500

    added avgt population method and avg number methjod for 2d arrays

--- src/array2d.java
+++ src/array2d.java
@@ -11,6 +11,7 @@ public class array2d {
             }
         }
         printArray();
+        System.out.println(sum2d());
 
     }
 
@@ -22,4 +23,25 @@ public class array2d {
             System.out.println();
         }
     }
+
+    public int sum2d() {
+        int sum = 0;
+        for (int r=0; r<numbers.length; r++) {
+            for (int c=0; c<numbers[r].length; c++) {
+                sum += numbers[r][c];
+            }
+        }
+        return sum;
+    }
+
+    public double average2d() {
+        int length = 0;
+        for (int r=0; r<numbers.length; r++) {
+            for (int c=0; c<numbers[r].length; c++) {
+                length++;
+            }
+        }
+        return (double) sum2d() /length;
+    }
+
 }
